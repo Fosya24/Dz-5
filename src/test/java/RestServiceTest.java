eb6268f6a4d146f5de2473db720b806380e76912
Fosya <napka1121@mail.ru>
2026-05-05 16:58:33 +0700

    добавлена параметризация

--- src/test/java/RestServiceTest.java
+++ src/test/java/RestServiceTest.java
@@ -1,20 +1,16 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import ru.netology.services.RestService;
 
 public class RestServiceTest {
 
     private final RestService service = new RestService();
 
-    @Test
-    public void shouldCalculateRest() {
-
-        int income = 10000;
-        int expenses = 3000;
-        int threshold = 20000;
-
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/rest.csv")
+    public void shouldCalculateRest(int income, int expenses, int threshold, int expected) {
         int actual = service.calculate(income, expenses, threshold);
-        int expected = 3;
 
         Assertions.assertEquals(expected, actual);
     }
